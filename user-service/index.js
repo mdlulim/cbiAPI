b00aa425021c074ca028f277848dabf12a17484d
Rodney <rodney.ncane@rewardsco.co.za>
2021-10-15 18:40:05 +0200

    updates to user-service

--- user-service/index.js
+++ user-service/index.js
@@ -1,18 +1,15 @@
-const express      = require('express');
-const bodyParser   = require('body-parser');
-const cors         = require('cors');
-const app          = express();
-const config       = require('./config');
-const router       = require('./router');
-const errorHandler = require('./helpers/errorHandler');
+const express    = require('express');
+const bodyParser = require('body-parser');
+const cors       = require('cors');
+const app        = express();
+const config     = require('./config');
+const router     = require('./router');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true,
 }));
 app.use(express.static('client'));
-app.use(errorHandler.notFound);
-app.use(errorHandler.internalServerError);
 app.use(cors());
 
 router.set(app);
--- user-service/services/user.js
+++ user-service/services/user.js
@@ -1,21 +1,33 @@
 // const sequelize = require('../config/db');
 const { User } = require('../models/user');
 
-// get all users
-const index = () => User.findAndCountAll({
-    order: [['created', 'DESC']],
-})
-.then(res => {
-    return {
-        status: "success",
-        data: {
-            count: res.count,
-            next: null,
-            previous: null,
-            results: res.rows,
-        }
-    };
-});
+/**
+ * Get all users
+ * @param {*} query 
+ * @returns 
+ */
+async function index(query) {
+    try {
+        const where = query || {};
+        const users = await User.findAndCountAll({
+            where,
+            order: [['created', 'DESC']],
+        });
+        const { count, rows } = users;
+        return {
+            success: true,
+            data: {
+                count,
+                next: null,
+                previous: null,
+                results: rows,
+            }
+        };
+    } catch (err) {
+        console.log(err);
+        res.send(err);
+    }
+};
 
 module.exports = {
     index
