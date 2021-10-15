5772496459fb7f1c04a289b957d0e226ca9b2032
Munyaradzi Muchenje <munyaradzim@emgage.com>
2021-10-15 10:59:15 +0200

    dbstring

--- user-service/config/index.js
+++ user-service/config/index.js
@@ -5,7 +5,7 @@ if (stage === 'dev') {
     // local
     config = {
         port: process.env.PORT || 8080,
-        dbConnectionString: 'postgresql://develop:6MO4vuw9fpn3dvfa@db-postgresql-ams3-49623-do-user-7844381-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require',
+        dbConnectionString: 'postgresql://develop:6MO4vuw9fpn3dvfa@private-db-postgresql-ams3-49623-do-user-7844381-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require',
         saltRounds: 2,
         jwtSecret: 'BbZJjyoXAdr8BUZuiKKARWimKfrSmQ6fv8kZ7OFfc',
         tokenExpireTime: '6h',
