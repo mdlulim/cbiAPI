f94d2f516a021814575860b4dc6b86892365aeae
Rodney <rodney.ncane@rewardsco.co.za>
2021-10-15 14:43:53 +0200

    added try catch to user-service

--- user-service/controllers/user.js
+++ user-service/controllers/user.js
@@ -1,14 +1,19 @@
 const userService = require('../services/user');
 
 function index(req, res){
-    return userService.index(req.query)
-    .then(data => res.send(data))
-    .catch(err => {
-        res.send({
-            success: false,
-            message: err.message,
+    try {
+        return userService.index(req.query)
+        .then(data => res.send(data))
+        .catch(err => {
+            res.send({
+                success: false,
+                message: err.message,
+            });
         });
-    });
+    } catch (err) {
+        console.log(err);
+        res.send(err);
+    }
 };
 
 module.exports = {
--- user-service/index.js
+++ user-service/index.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-var cors = require('cors');
-const app = express();
-const config = require('./config');
-const router = require('./router');
+const express      = require('express');
+const bodyParser   = require('body-parser');
+const cors         = require('cors');
+const app          = express();
+const config       = require('./config');
+const router       = require('./router');
+const errorHandler = require('./helpers/errorHandler');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true,
 }));
 app.use(express.static('client'));
+app.use(errorHandler.notFound);
+app.use(errorHandler.internalServerError);
 app.use(cors());
 
 router.set(app);
