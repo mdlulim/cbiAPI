cc53ca736410d6a2790b80b1da9c51595ca88608
Munyaradzi Muchenje <munyaradzim@emgage.com>
2021-10-15 10:13:15 +0200

    add private string

--- user-service/config/index.js
+++ user-service/config/index.js
@@ -5,7 +5,7 @@ if (stage === 'dev') {
     // local
     config = {
         port: process.env.PORT || 8080,
-        dbConnectionString: 'postgresql://doadmin:show-password@private-db-postgresql-ams3-49623-do-user-7844381-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require',
+        dbConnectionString: 'postgresql://doadmin:bMs2X9InUxyC9DTG@db-postgresql-ams3-49623-do-user-7844381-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require',
         saltRounds: 2,
         jwtSecret: 'BbZJjyoXAdr8BUZuiKKARWimKfrSmQ6fv8kZ7OFfc',
         tokenExpireTime: '6h',
