5fad1ff850410de3eed6f35041ed19e3dd108334
Munyaradzi Muchenje <munyaradzim@emgage.com>
2021-10-16 19:22:32 +0200

    added Dockkerfiles

--- admin-service/config/index.js
+++ admin-service/config/index.js
@@ -4,7 +4,7 @@ var config = {};
 if (stage === 'dev') {
     // local
     config = {
-        port: process.env.PORT || 8082,
+        port: process.env.PORT || 8080,
         dbConnectionString: 'postgresql://doadmin:bMs2X9InUxyC9DTG@db-postgresql-ams3-49623-do-user-7844381-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require',
         saltRounds: 2,
         jwtSecret: 'BbZJjyoXAdr8BUZuiKKARWimKfrSmQ6fv8kZ7OFfc',
