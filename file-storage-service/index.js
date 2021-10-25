3a4dd757ea82dd31777d1d8afafe6090f126cf9e
Munyaradzi Muchenje <munyaradzim@emgage.com>
2021-10-25 14:44:10 +0200

    added apm

--- file-storage-service/index.js
+++ file-storage-service/index.js
@@ -2,7 +2,7 @@
 var apm = require('elastic-apm-node').start({
     serviceName: '',
     secretToken: 'K1dPeS59y0hO980e9d0ed4pI',
-    serverUrl: 'http://apm-server-apm-http:8200',
+    serverUrl: 'https://apm-server-apm-http.default.svc.cluster.local:8200',
     environment: 'production'
     })
 
