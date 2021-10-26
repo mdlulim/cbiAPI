a66eb792c083d2f9bc48a5b6211c699035322c11
Rodney <rodney.ncane@rewardsco.co.za>
2021-10-26 19:34:00 +0200

    updated routes

--- admin-service/router.js
+++ admin-service/router.js
@@ -12,70 +12,70 @@ module.exports.set = app => {
      * 
      * Create a user.
      */
-    app.post('/admin/users', authMiddleware.checkAuth, userController.create);
+    app.post('/users', authMiddleware.checkAuth, userController.create);
 
     /**
      * List Users
      * 
      * Get a list of users belonging to CBI.
      */
-    app.get('/admin/users', authMiddleware.checkAuth, userController.index);
+    app.get('/users', authMiddleware.checkAuth, userController.index);
 
     /**
      * Retrieve User
      * 
      * Retrieve a company’s user (profile).
      */
-    app.get('/admin/users/:id', authMiddleware.checkAuth, userController.show);
+    app.get('/users/:id', authMiddleware.checkAuth, userController.show);
 
     /**
      * Update User
      * 
      * Update user details.
      */
-    app.put('/admin/users/:id', authMiddleware.checkAuth, userController.update);
+    app.put('/users/:id', authMiddleware.checkAuth, userController.update);
 
     /**
      * Archive User
      * 
      * Archived company's user.
      */
-    app.put('/admin/users/:id/archive', authMiddleware.checkAuth, userController.archive);
+    app.put('/users/:id/archive', authMiddleware.checkAuth, userController.archive);
 
     /**
      * Block User
      * 
      * Block company's user.
      */
-    app.put('/admin/users/:id/block', authMiddleware.checkAuth, userController.block);
+    app.put('/users/:id/block', authMiddleware.checkAuth, userController.block);
 
     /**
      * Retrieve User' Referrals
      * 
      * Retrieve a company’s user referrals.
      */
-    app.get('/admin/users/:id/referrals', authMiddleware.checkAuth, userController.referrals);
+    app.get('/users/:id/referrals', authMiddleware.checkAuth, userController.referrals);
 
     /**
      * Retrieve User' Products
      * 
      * Retrieve a company’s user products.
      */
-    app.get('/admin/users/:id/products', authMiddleware.checkAuth, userController.products);
+    app.get('/users/:id/products', authMiddleware.checkAuth, userController.products);
 
     /**
      * Retrieve User' Transactions
      * 
      * Retrieve a company’s user transactions.
      */
-    app.get('/admin/users/:id/transactions', authMiddleware.checkAuth, userController.transactions);
+    app.get('/users/:id/transactions', authMiddleware.checkAuth, userController.transactions);
 
     /**
      * List User Products
      * 
      * Get a list of products belonging to CBI's user.
      */
-    // app.get('/admin/users/:id/products', authMiddleware.checkAuth, userController.products);
+    // app.get('/users/:id/products', authMiddleware.checkAuth, userController.products);
 
     /**
      * Groups
@@ -86,95 +86,95 @@ module.exports.set = app => {
      * view, add, edit or delete data from the system
      * via admin endpoints.
      */
-    app.post('/admin/groups', authMiddleware.checkAuth, groupController.create);
-    app.get('/admin/groups', authMiddleware.checkAuth, groupController.index);
-    app.get('/admin/groups/:id', authMiddleware.checkAuth, groupController.show);
-    app.put('/admin/groups/:id', authMiddleware.checkAuth, groupController.update);
-    app.delete('/admin/groups/:id', authMiddleware.checkAuth, groupController.destroy);
+    app.post('/groups', authMiddleware.checkAuth, groupController.create);
+    app.get('/groups', authMiddleware.checkAuth, groupController.index);
+    app.get('/groups/:id', authMiddleware.checkAuth, groupController.show);
+    app.put('/groups/:id', authMiddleware.checkAuth, groupController.update);
+    app.delete('/groups/:id', authMiddleware.checkAuth, groupController.destroy);
 
     /**
      * Create Product
      * 
      * Create a single product belonging to CBI.
      */
-    app.post('/admin/products', authMiddleware.checkAuth, productController.create);
+    app.post('/products', authMiddleware.checkAuth, productController.create);
 
     /**
      * List Products
      * 
      * Get a list of products belonging to CBI.
      */
-    app.get('/admin/products', authMiddleware.checkAuth, productController.index);
+    app.get('/products', authMiddleware.checkAuth, productController.index);
 
     /**
      * Retrieve Product
      * 
      * Retrieve a company’s product.
      */
-    app.get('/admin/products/:id', authMiddleware.checkAuth, productController.show);
+    app.get('/products/:id', authMiddleware.checkAuth, productController.show);
 
     /**
      * Update Product
      * 
      * Update company’s product details.
      */
-    app.put('/admin/products/:id', authMiddleware.checkAuth, productController.update);
+    app.put('/products/:id', authMiddleware.checkAuth, productController.update);
 
     /**
      * Delete Product
      * 
      * Delete company’s product.
      */
-    app.delete('/admin/products/:id', authMiddleware.checkAuth, productController.destroy);
+    app.delete('/products/:id', authMiddleware.checkAuth, productController.destroy);
 
     /**
      * List Transactions
      * 
      * Get a list of transactions belonging to CBI.
      */
-    app.get('/admin/transactions', authMiddleware.checkAuth, transactionController.index);
+    app.get('/transactions', authMiddleware.checkAuth, transactionController.index);
 
     /**
      * Retrieve Transaction
      * 
      * Retrieve a company’s transaction.
      */
-    app.get('/admin/transactions/:id', authMiddleware.checkAuth, transactionController.show);
+    app.get('/transactions/:id', authMiddleware.checkAuth, transactionController.show);
 
     /**
      * List Currencies
      * 
      * Get a list of currencies belonging to CBI.
      */
-    app.get('/admin/currencies', authMiddleware.checkAuth, currencyController.index);
+    app.get('/currencies', authMiddleware.checkAuth, currencyController.index);
 
     /**
      * Retrieve Currency
      * 
      * Retrieve a company’s currency.
      */
-    app.get('/admin/currencies/:code', authMiddleware.checkAuth, currencyController.show);
+    app.get('/currencies/:code', authMiddleware.checkAuth, currencyController.show);
 
     /**
      * Update Currency
      * 
      * Update company’s currency details.
      */
-    app.put('/admin/currencies/:code', authMiddleware.checkAuth, currencyController.update);
+    app.put('/currencies/:code', authMiddleware.checkAuth, currencyController.update);
 
     /**
      * Delete Currency
      * 
      * Delete company’s currency.
      */
-    app.delete('/admin/currencies/:code', authMiddleware.checkAuth, currencyController.destroy);
+    app.delete('/currencies/:code', authMiddleware.checkAuth, currencyController.destroy);
 
     /**
      * List Countries
      * 
      * Get a list of countries belonging to CBI.
      */
-    app.get('/admin/countries', authMiddleware.checkAuth, countryController.index);
-    app.put('/admin/countries/:id/blacklist', authMiddleware.checkAuth, countryController.blacklist);
-    app.put('/admin/countries/:id/unblacklist', authMiddleware.checkAuth, countryController.unblacklist);
+    app.get('/countries', authMiddleware.checkAuth, countryController.index);
+    app.put('/countries/:id/blacklist', authMiddleware.checkAuth, countryController.blacklist);
+    app.put('/countries/:id/unblacklist', authMiddleware.checkAuth, countryController.unblacklist);
 };
