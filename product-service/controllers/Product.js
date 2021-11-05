fb9552e66d4dbe2bdb7f20a9614f35a083b292d8
Rodney <rodney.ncane@rewardsco.co.za>
2021-11-05 10:37:25 +0200

    wc subscribe

--- product-service/controllers/Product.js
+++ product-service/controllers/Product.js
@@ -1,5 +1,6 @@
 const activityService = require('../services/Activity');
 const productService  = require('../services/Product');
+const userService = require('../services/User');
 
 async function overview(req, res){
     try {
@@ -39,19 +40,36 @@ async function subscribe(req, res){
             user_id: req.user.id,
             product_id: req.body.id,
         };
+
+        // subscribe
         await productService.subscribe(data);
+
+        let description = `${req.user.first_name} bought a product (${product.title})`;
+        if (req.body.wc) {
+            description = `${req.user.first_name} became a Wealth Creator`;
+        }
+
+        // log activity
         await activityService.addActivity({
             user_id: req.user.id,
-            action: `${req.user.group_name}.products.buy`,
-            description: `${req.user.first_name} bought a product (${product.title})`,
+            action: req.body.wc ? 'wealth-creator.subscribe' : `${req.user.group_name}.products.buy`,
             section: 'Products',
             subsection: 'Buy',
+            description,
             ip: null,
             data: {
                 ...data,
                 product,
             },
         });
+
+        // update user group (if wealth-creator subscription)
+        if (req.body.wc) {
+            await userService.update(req.user.id, {
+                group_id: 'c85ef2f9-d1dc-451d-b36d-9f0b111c1882',
+            });
+        }
+
         return res.send({
             success: true,
         });
