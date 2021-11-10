add1a05f466415d1ffb1b3879f08429ea99d58a1
Munyaradzi Muchenje <munyaradzim@emgage.com>
2021-11-10 18:00:45 +0200

    removed buddy logic

--- transaction-service/router.js
+++ transaction-service/router.js
@@ -39,17 +39,4 @@ module.exports.set = app => {
      */
     app.get('/fees/:tx_type/:subtype', authMiddleware.checkAuth, feeController.show);
 
-
-    // buddyAPI Routes
-    app.get("/buddy/lookup-balance", authMiddleware.checkAuth, buddyAPIController.lookupBalance);
-    app.get("/buddy/lookup-account", authMiddleware.checkAuth, buddyAPIController.lookupAccount);
-    app.get("/buddy/lookup-transactions", authMiddleware.checkAuth, buddyAPIController.lookupTransaction);
-    app.post("/buddy/eventtransfer", authMiddleware.checkAuth, buddyAPIController.eventTransfer);
-
-    // buddyAccount Routes
-    app.get("/buddy", authMiddleware.checkAuth, buddyAccountController.index);
-    app.post("/buddy", authMiddleware.checkAuth, buddyAccountController.store);
-    app.get("/buddy/:buddyId", authMiddleware.checkAuth, buddyAccountController.show);
-    app.put("/buddy/:buddyId", authMiddleware.checkAuth, buddyAccountController.updateBuddy);
-    app.delete("/buddy/:buddyId", authMiddleware.checkAuth, buddyAccountController.destroy);
 };
