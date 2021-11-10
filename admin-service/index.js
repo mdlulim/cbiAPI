a99fc9787c67c181186f150af1aba6c233b761ff
Munyaradzi Muchenje <munyaradzim@emgage.com>
2021-11-10 16:06:38 +0200

    apm

--- admin-service/index.js
+++ admin-service/index.js
@@ -1,11 +1,11 @@
-// // add application performance monitoring 
-// var apm = require('elastic-apm-node').start({
-//     serviceName: 'admin-service',
-//     secretToken: 'K1dPeS59y0hO980e9d0ed4pI',
-//     serverUrl: 'https://apm-server-apm-http.default.svc.cluster.local:8200',
-//     environment: 'development',
-//     verifyServerCert: false
-//     });
+// add application performance monitoring 
+var apm = require('elastic-apm-node').start({
+    serviceName: 'admin-service',
+    secretToken: 'K1dPeS59y0hO980e9d0ed4pI',
+    serverUrl: 'https://apm-server-apm-http.default.svc.cluster.local:8200',
+    environment: 'development',
+    verifyServerCert: false
+    });
     
 const express = require('express');
 const bodyParser = require('body-parser');
