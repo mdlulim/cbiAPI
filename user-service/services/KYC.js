1e84f04b41aa52d780c0e78ca0f0037e440f25e3
Pelema <abpalema@gmail.com>
2021-11-11 13:33:21 +0200

    changed batch create to transaction

--- user-service/services/KYC.js
+++ user-service/services/KYC.js
@@ -3,8 +3,19 @@ const { KYC } = require('../models/KYC');
 
 async function capture(data) {
     try {
-        return KYC.bulkCreate(data);
+
+        const result = await sequelize.transaction(async (t) => {
+
+            let res = null
+            data.forEach(async(level) => {
+               res = await KYC.upsert(level, {transaction: t} )
+            });
+            // return KYC.bulkCreate(data);
+            return res
+        });
+
         
+
     } catch (error) {
         console.error(error.message || null);
         throw new Error('Could not process your request');
