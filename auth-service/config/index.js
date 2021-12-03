8f8193de07abc4f8ca159311dfe37d17f32980c1
Munyaradzi Muchenje <munyaradzim@emgage.com>
2021-12-03 10:08:58 +0200

    added envs

--- auth-service/config/index.js
+++ auth-service/config/index.js
@@ -1,6 +1,7 @@
-
 if (process.env.NODE_ENV === 'production') {
     module.exports = require('./prod')
+} else if (process.env.NODE_ENV === 'qa') {
+    module.exports = require('./qa')
 } else {
     module.exports = require('./dev')
 }
---
+++ buddy-service/config/qa.js
@@ -0,0 +1,31 @@
+config = {
+    port: process.env.PORT || 8080,
+    dbConnectionString: 'postgresql://doadmin:bMs2X9InUxyC9DTG@db-postgresql-ams3-49623-do-user-7844381-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require',
+    saltRounds: 2,
+    jwtSecret: 'BbZJjyoXAdr8BUZuiKKARWimKfrSmQ6fv8kZ7OFfc',
+    tokenExpireTime: '6h',
+    tokenExpireHours: 6,
+    baseurl: {
+        admin: 'https://qa.cbiglobal.io',
+        frontend: 'https://dev.qa.cbiglobal.io',
+    },
+    mail: {
+        smtp: {
+            host: 'mail.cbiglobal.io',
+            port: 465,
+            secure: true,
+            auth: {
+                user: 'no-reply@cbiglobal.io',
+                pass: 'ocJ~$m[NTj#N',
+            }
+        }
+    },
+    buddy: {
+        staging: {
+            base_url: 'https://staging.buddy.na/api/v2/services',
+            authenticationToken: Buffer.from("6b05e06a-cdcb-41dd-8e01-8dcf551e726d").toString('base64')
+        }
+    }
+}
+
+module.exports = config;
