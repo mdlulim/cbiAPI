a42c27f8f1616b97ba9d4221c973b1381e251586
mdlulim <mduduzmdluli@gmail.com>
2021-12-18 22:18:53 +0200

    admin general setting

---
+++ admin-service/controllers/Setting.js
@@ -0,0 +1,109 @@
+const activityService = require('../services/Activity');
+const settingService = require('../services/Setting');
+
+function permakey(title) {
+    return title.split(' ')
+        .join('-')
+        .trim()
+        .toLowerCase();
+}
+
+async function create(req, res){
+    try{
+        const data = req.body;
+        data.captured_by = req.user.id;
+        data.key = permakey(data.title);
+        // check product by unique permakey/code
+        const setting = await settingService.findByKey(data.key);
+        if (setting && setting.id) {
+            return res.send({
+                success: false,
+                message: 'Validation error. Same setting title already exists'
+            });
+        }
+
+         settingService.create(data);
+         activityService.addActivity({
+            user_id: req.user.id,
+            action: `${req.user.group_name}.settings.add`,
+            description: `${req.user.group_name} added a new settings (${data.title})`,
+            section: 'Settings',
+            subsection: 'Add',
+            ip: null,
+            data,
+        });
+        return res.send({
+            success: true,
+        });
+    }catch (error) {
+        console.log(error)
+        return res.status(500).send({
+            success: false,
+            message: 'Could not process request'
+        });
+    }
+    
+};
+
+async function index(req, res) {
+    try {
+        return settingService.index(req.query)
+        .then(data => res.send(data))
+        .catch(err => {
+            res.send({
+                success: false,
+                message: err.message,
+            });
+        });
+    } catch (error) {
+        return res.send({
+            success: false,
+            message: 'Could not process request 2'
+        });
+    }
+}
+
+async function show(req, res) {
+    try {
+        return settingService.show(req.params.id)
+        .then(data => res.send(data))
+        .catch(err => {
+            res.send({
+                success: false,
+                message: err.message,
+            });
+        });
+    } catch (error) {
+        return res.send({
+            success: false,
+            message: 'Could not process request'
+        });
+    }
+}
+
+async function update(req, res) {
+    try {
+        return settingService.update(req.params.id, req.body)
+        .then(() => res.send({ success: true }))
+        .catch(err => {
+            res.send({
+                success: false,
+                message: err.message,
+            });
+        });
+    } catch (error) {
+        return res.send({
+            success: false,
+            message: 'Could not process request'
+        });
+    }
+}
+
+
+
+module.exports = {
+    create,
+    index,
+    show,
+    update,
+}
---
+++ admin-service/services/Setting.js
@@ -0,0 +1,134 @@
+const { Setting }  = require('../models/Setting');
+
+
+async function create(data) {
+    try {
+        return Setting.create(data);
+    } catch (error) {
+        console.error(error.message || null);
+        throw new Error('Could not process your request');
+    }
+}
+
+/**
+ * List Settings
+ * 
+ * Get a list of settings belonging to CBI.
+ * 
+ * @param {object} query
+ * @returns
+ */
+async function index(query) {
+    try {
+        const { offset, limit } = query;
+        const where = query || {};
+        const groupWhere = {};
+
+        delete where.offset;
+        delete where.limit;
+
+        if (where.group) {
+            groupWhere.name = where.group;
+            delete where.group;
+        }
+
+        const settings = await Setting.findAndCountAll({
+            attributes: [
+                'id',
+                'title',
+                'category',
+                'key',
+                'value',
+                'company_id',
+                'subcategory',
+            ],
+            where,
+            offset: offset || 0,
+            limit: limit || 100,
+        });
+        const { count, rows } = settings;
+        return {
+            success: true,
+            data: {
+                count,
+                next: null,
+                previous: null,
+                results: rows,
+            }
+        };
+    } catch (error) {
+        console.error(error.message || null);
+        throw new Error('Could not process your request');
+    }
+}
+
+/**
+ * Retrieve Setting
+ * 
+ * Retrieve a company’s setting.
+ * 
+ * @param {string} id 
+ * @returns 
+ */
+async function show(id) {
+    try {
+        const setting = await Setting.findOne({
+            attributes: [
+                'id',
+                'title',
+                'category',
+                'key',
+                'value',
+                'company_id',
+                'subcategory',
+            ],
+            where: { id },
+        });
+        return {
+            success: true,
+            data: setting,
+        };
+    } catch (error) {
+        console.error(error.message || null);
+        throw new Error('Could not process your request');
+    }
+}
+
+/**
+ * 
+ * Update Setting
+ * 
+ * Update company’s setting details.
+ * 
+ * @param {string} id
+ * @param {string} data 
+ * @returns 
+ */
+async function update(id, data) {
+    try {
+        return Setting.update(data, { where: { id } });
+    } catch (error) {
+        console.error(error.message || null);
+        throw new Error('Could not process your request');
+    }
+}
+
+async function findByKey(key) {
+    try {
+        //console.log(key)
+        return  await Setting.findOne({
+            where: { key: key }
+        });
+    } catch (error) {
+        console.error(error.message || null);
+        throw new Error('Could not process your request');
+    }
+}
+
+module.exports = {
+    create,
+    index,
+    show,
+    update,
+    findByKey
+}
