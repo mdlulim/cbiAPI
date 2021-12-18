29a59e8a96f052efdf856207d9f825ee7f694e58
munyac <30975277+mcmuchenje@users.noreply.github.com>
2021-12-18 11:50:02 +0200

    Update dev.js

--- admin-service/config/dev.js
+++ admin-service/config/dev.js
@@ -1,5 +1,5 @@
 config = {
-    port: process.env.PORT || 7000,
+    port: process.env.PORT || 8080,
     dbConnectionString: 'postgresql://doadmin:bMs2X9InUxyC9DTG@db-postgresql-ams3-49623-do-user-7844381-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require',
     saltRounds: 2,
     jwtSecret: 'BbZJjyoXAdr8BUZuiKKARWimKfrSmQ6fv8kZ7OFfc',
