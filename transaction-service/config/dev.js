3dfa5234742ec3b9c7cd51e32b9bdef823c6684b
Munyaradzi Muchenje <munyaradzim@emgage.com>
2021-12-19 15:34:12 +0200

    added port numbers

--- transaction-service/config/dev.js
+++ transaction-service/config/dev.js
@@ -1,6 +1,6 @@
 config = {
     cryptrKey: 'LfFLf8cAAABwvgp',
-    port: process.env.PORT || 8080,
+    port: (process.env.NODE_ENV === 'develop') ? 8080 : 8089,
     dbConnectionString: 'postgresql://doadmin:bMs2X9InUxyC9DTG@db-postgresql-ams3-49623-do-user-7844381-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require',
     saltRounds: 2,
     jwtSecret: 'BbZJjyoXAdr8BUZuiKKARWimKfrSmQ6fv8kZ7OFfc',
