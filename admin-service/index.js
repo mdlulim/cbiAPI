1045ebc2347eba46616853ab59cc933bfcc975ba
Munyaradzi Muchenje <munyaradzim@emgage.com>
2021-12-20 14:11:03 +0200

    created swagger docs

--- admin-service/index.js
+++ admin-service/index.js
@@ -9,10 +9,50 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 var cors = require('cors');
 const app = express();
 const config = require('./config');
 const router = require('./router');
+
+// Extended: https://swagger.io/specification/#infoObject
+const swaggerOptions = {
+    swaggerDefinition: {
+      info: {
+        version: "1.0.0",
+        title: "admin-service API",
+        description: "This is a REST API application made with Express. It retrieves Buddy information related to CBI.",
+        contact: {
+            name: 'API Support',
+            url: 'https://cbiglobal.io',
+            email: 'support@cbiglobal.io'
+        },
+        servers: [
+            {
+                url: 'http://dev.cbiglobal.io/v1/admin',
+                description: 'Development server',
+            },
+            {
+                url: 'http://dev.qa.cbiglobal.io/v1/admin',
+                description: 'QA server',
+            },
+            {
+                url: 'http://dev.release.cbiglobal.io/v1/admin',
+                description: 'Staging server',
+            },
+            {
+                url: 'http://api.cbiglobal.io/v1/admin',
+                description: 'Production server',
+            },
+        ]
+      }
+    },
+    apis: ["router.js"]
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
