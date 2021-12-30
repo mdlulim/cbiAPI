93d941c0f7732ef38a266324e652e7c0a8685602
Pelema <abpalema@gmail.com>
2021-12-30 19:18:23 +0200

    Fixed file upload bug

--- file-storage-service/controllers/Document.js
+++ file-storage-service/controllers/Document.js
@@ -3,12 +3,7 @@ const fileUploadService = require('../services/FileUpload');
 
 async function upload(request, response) {
     try {
-        const data = fileUploadService.uploader(request, response);
-        if (!data.success) {
-            return response.status(403).send(data)
-        }
-
-        return response.status(200).send(data);
+        return fileUploadService.uploader(request, response);
 
     } catch (error) {
         console.log(error.message || null)
