c9dd6a1466795dd24afd253e7a5bffee3dade398
Pelema <abpalema@gmail.com>
2022-02-05 00:01:11 +0200

    Fix missing quotes bug

--- user-service/lib/emailTemplates/index.js
+++ user-service/lib/emailTemplates/index.js
@@ -28,7 +28,7 @@ const autoRenewStatusChange = data => {
                 line-height: 1.5;">
             <div style="padding: 20px 0; margin-bottom: 20px; background-color: #2D3357;  background-image: linear-gradient(310deg,#141727,#3a416f);  color: white; ">
                 <div style="align-items: center; text-align: center;">
-                    <img src="https://cdn-cbigold.ams3.digitaloceanspaces.com/public/email/CDC3837FF1DF9ADC1FF459D0278FD.png" height: 50px alt="">
+                    <img src="https://cdn-cbigold.ams3.digitaloceanspaces.com/public/email/CDC3837FF1DF9ADC1FF459D0278FD.png" height: "50px" alt="">
                 </div>
                 <!-- Email topic -->
                 <p style="line-height: 2; text-align: center;">
@@ -124,7 +124,7 @@ const updateNotification = data => {
                 line-height: 1.5;">
             <div style="padding: 20px 0; margin-bottom: 20px; background-color: #2D3357;  background-image: linear-gradient(310deg,#141727,#3a416f);  color: white; ">
                 <div style="align-items: center; text-align: center;">
-                    <img src="https://cdn-cbigold.ams3.digitaloceanspaces.com/public/email/CDC3837FF1DF9ADC1FF459D0278FD.png" height: 50px alt="">
+                    <img src="https://cdn-cbigold.ams3.digitaloceanspaces.com/public/email/CDC3837FF1DF9ADC1FF459D0278FD.png" height: "50px" alt="">
                 </div>
                 <!-- Email topic -->
                 <p style="line-height: 2; text-align: center;">
