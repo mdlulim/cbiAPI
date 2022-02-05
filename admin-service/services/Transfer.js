777a47446c277ce53a2ded1e96f49c4d821781c5
Munyaradzi Muchenje <munyaradzim@emgage.com>
2022-02-05 23:10:07 +0200

    added changes

--- admin-service/services/Transfer.js
+++ admin-service/services/Transfer.js
@@ -1,5 +1,6 @@
 const moment = require('moment');
 const { Account } = require('../models/Account');
+const { Transfer } = require('../models/Transfer');
 
 async function transfer(data) {
     try {
@@ -27,6 +28,12 @@ async function transfer(data) {
                 where: {
                     id: '3cf7d2c0-80e1-4264-9f2f-6487fd1680c2'
                 }
+            })
+
+            await Transfer.create({
+                user_id: data.user_id,
+                note: `account ${data.user_id} has been credited with ${parseFloat(data.amount)} CBI on ${moment().format()}`,
+                amount: data.amount,
             })
 
             return {
