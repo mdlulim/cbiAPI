5df1108b6a06957a1508b92c3f234de2cd79dba8
mdlulim <mduduzmdluli@gmail.com>
2022-02-06 16:20:55 +0200

    debit or credit member

--- admin-service/controllers/TransferController.js
+++ admin-service/controllers/TransferController.js
@@ -1,8 +1,20 @@
 const transferService = require('../services/Transfer');
+const transactionService = require('../services/Transaction');
+
+const getTxid = (subtype, autoid) => {
+    return subtype.substr(0, 3).toUpperCase() + autoid.toString();
+};
 
 async function transfer(req, res){
     try {
-        const response = await transferService.transfer(req.body);
+        const admin_user_id = req.user.id;
+        const response = await transferService.transfer(req.body, admin_user_id,);
+        const subtype= response.transaction.dataValues.subtype;
+        const auto_id= response.transaction.dataValues.auto_id;
+        const txid = getTxid(subtype, auto_id);
+        const id = response.transaction.dataValues.id;
+        let transData = { txid: txid, status: 'Completed' }
+        await transactionService.update(transData, id);
         return res.send({
             success: true,
             response
@@ -17,7 +29,7 @@ async function transfer(req, res){
 
 async function history(req, res){
     try {
-        const response = await transferService.history(req.body.user_id);
+        const response = await transferService.history(req.query);
         return res.send({
             success: true,
             response
