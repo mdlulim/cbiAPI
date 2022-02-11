13213d037eb7d9d906b09459eab0d4deea1c2425
Munyaradzi Muchenje <munyaradzim@emgage.com>
2022-02-11 20:39:21 +0200

    added connection string

--- cron-service/config/release.js
+++ cron-service/config/release.js
@@ -1,6 +1,6 @@
 config = {
     port: process.env.NODE_ENV ? 8080 : 8086,
-    dbConnectionString: 'postgresql://doadmin:CtTyMZ7Vj5pcX0kc@private-qa-db-postgresql-ams3-38151-do-user-7844381-0.b.db.ondigitalocean.com:25061/release-connection_pool?sslmode=require',
+    dbConnectionString: 'postgresql://doadmin:CtTyMZ7Vj5pcX0kc@qa-db-postgresql-ams3-38151-do-user-7844381-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require',
     saltRounds: 2,
     jwtSecret: 'BbZJjyoXAdr8BUZuiKKARWimKfrSmQ6fv8kZ7OFfc',
     tokenExpireTime: '6h',
